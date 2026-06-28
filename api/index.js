e336653a501905f9a926fe7fedde4a7d9437fd8e
PerezMigue1 <20230091@uthh.edu.mx>
2026-06-28 02:12:01 -0600

    cambios88

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
+console.log('[VERCEL ENV CHECK]', {
+    hasClientId: Boolean(process.env.ALEXA_CLIENT_ID),
+    hasMongoUri: Boolean(process.env.MONGODB_URI),
+    hasJwtSecret: Boolean(process.env.JWT_ACCESS_SECRET),
+    nodeEnv: process.env.NODE_ENV
+});
+
 const app = require('../src/app');
 const connectDatabase = require('../src/config/database');
 
